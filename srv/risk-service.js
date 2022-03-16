3e520cca72c92381fd2d95bd70518343266edde8
liqing <616610062@qq.com>
2022-03-16 17:06:58 +0800

    report app with business logic

---
+++ srv/risk-service.js
@@ -0,0 +1,30 @@
+
+const cds = require('@sap/cds')
+
+/**
+ * Implementation for Risk Management service defined in ./risk-service.cds
+ */
+module.exports = cds.service.impl(async function() {
+    this.after('READ', 'Risks', risksData => {
+        const risks = Array.isArray(risksData) ? risksData : [risksData];
+        risks.forEach(risk => {
+            if (risk.impact >= 100000) {
+                risk.criticality = 1;
+            } else {
+                risk.criticality = 2;
+            }
+        });
+    });
+
+    this.after('READ','Reports',reportsData =>{
+        const reports = Array.isArray(reportsData) ? reportsData : [reportsData];
+        reports.forEach(report => {
+            if (report.result < 3){
+                report.criticality = 1;
+            }else{
+                report.criticality = 3;
+            }
+        })
+    })
+    
+});
